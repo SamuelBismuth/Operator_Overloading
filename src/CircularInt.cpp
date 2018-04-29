6b7b5b22a66279ead6f1e7f51c56b569546a4afb
Samuel <sam101@hotmail.fr>
2018-04-29 16:21:46 +0300

    Final commit

--- src/CircularInt.cpp
+++ src/CircularInt.cpp
@@ -882,8 +882,9 @@ CircularInt operator- (const int decrement, const CircularInt& circularInt) {
  * Complexity : O(1).
  */
 CircularInt operator/ (const int divisor, const CircularInt& circularInt) {
+    if (divisor % circularInt.currentNumber != 0)
+        throw string(NotDivisible(circularInt, divisor).what());
     CircularInt answer = circularInt;
-    answer.isDivisible(divisor);
     answer.currentNumber /= divisor;
     answer.currentNumber = answer.moduloOperation();
     return answer;
