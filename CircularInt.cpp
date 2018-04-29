4b2a4e1adde8ca3387c4334956e59dbf1ea90457
Samuel <sam101@hotmail.fr>
2018-04-29 16:35:22 +0300

    Final commit

--- CircularInt.cpp
+++ CircularInt.cpp
@@ -666,7 +666,7 @@ CircularInt& CircularInt::operator*= (const int multiplier) {
  * \return the object CircularInt after the modification (if it's happen).
  */
 CircularInt& CircularInt::operator/= (const CircularInt circularInt) {
-    isDivisible(circularInt.currentNumber);
+    //isDivisible(circularInt.currentNumber);
     currentNumber /= circularInt.currentNumber;
     currentNumber = moduloOperation();
     return *this;
@@ -678,7 +678,7 @@ CircularInt& CircularInt::operator/= (const CircularInt circularInt) {
  * \return the object CircularInt after the modification (if it's happen).
  */
 CircularInt& CircularInt::operator/= (const int divisor) {
-    isDivisible(divisor);
+    //isDivisible(divisor);
     currentNumber /= divisor;
     currentNumber = moduloOperation();
     return *this;
@@ -882,6 +882,8 @@ CircularInt operator- (const int decrement, const CircularInt& circularInt) {
  * Complexity : O(1).
  */
 CircularInt operator/ (const int divisor, const CircularInt& circularInt) {
+    //if (divisor % circularInt.currentNumber != 0)
+      //  throw string(NotDivisible(circularInt, divisor).what());
     CircularInt answer = circularInt;
     answer.currentNumber = divisor / answer.currentNumber;
     answer.currentNumber = answer.moduloOperation();
